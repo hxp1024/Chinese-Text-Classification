4521bfadc388af0b58cc83e4944e4253a90800d7
hxp1024 <hxp1024@163.com>
2021-05-23 21:55:55 +0800

    .

--- models/TextRNN.py
+++ models/TextRNN.py
@@ -20,7 +20,8 @@ class Config(object):
         self.embedding_pretrained = torch.tensor(
             np.load(dataset + '/data/' + embedding)["embeddings"].astype('float32'))\
             if embedding != 'random' else None                                       # 预训练词向量
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
+        # self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
+        self.device = torch.device('cpu')   # 设备
 
         self.dropout = 0.5                                              # 随机失活
         self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
